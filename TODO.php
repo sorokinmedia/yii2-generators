1f420063a7d9d2f99b26845661769d7bb5fb7973
ma3obblu <Ma3oBblu@gmail.com>
2018-09-02 03:59:16 +0400

    [todo] founded bugs to fix

--- TODO.php
+++ TODO.php
@@ -1,2 +1,6 @@
 <?php
-//TODO: nothing todo ;-)
+//TODO: entity generator закрыть скобку в основном классе
+//TODO: form generator json поля
+//TODO: handler implement пустой при всех выбранных галочках
+//TODO: handler action/interfaces при всех выбранных галочках не создаются файлы
+//TODO: entity generator в АР модель добавить Timestamp генератор
